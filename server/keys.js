4c56d93e79ed66ba575854a240b53e0b6c85bed4
Tina <tinamurmu111@gmail.com>
2024-01-14 13:17:57 +0530

    first commit

---
+++ server/keys.js
@@ -0,0 +1,8 @@
+module.exports={
+    MONGOURI:"mongodb+srv://Tina:febygAQryCzXjDJn@cluster0.q1zp6fa.mongodb.net/?retryWrites=true&w=majority",
+    JWT_SECRET: "JLAJLJLLLJLJLOJOEBJF"
+}
+
+
+
+// ez8Mh72Hr8rBybjL
